b066d002405d01dc7b14783ca4dd8a24e387c84c
SanitaGasanov <sanitagasanov12@gmail.com>
2026-06-15 03:11:58 +0300

    ok

--- utils.h
+++ utils.h
@@ -1,67 +1,17 @@
-#include "utils.h"
-//////////////////////////////////////////////////
-bool g_use_colors = false;
-bool g_is_silent_mode = false;
+#pragma once
+/////////////////////////////////////////////////////////
+#include <windows.h> // Required for Windows API console functions
+#include <cstdlib> // For system("cls")
+#include "Enums.h"
+///////////////////////////////////////////////////////////////
 
-void gotoxy(int x, int y) {
-    if (g_is_silent_mode) return;
-    cout.flush();
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-//////////////////////////////////////////////////////
 
-void hideCursor()
-{
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO curInfo;
-    GetConsoleCursorInfo(hStdOut, &curInfo);
-    curInfo.bVisible = FALSE; // Set to TRUE to make it visible
-    SetConsoleCursorInfo(hStdOut, &curInfo);
-}
-/////////////////////////////////////////////////////////////////
-void cls() {
-    if (g_is_silent_mode) return;
-    system("cls");
-}
-//////////////////////////////////////////////////////
-void setTextColor(Color color) 
-{
-    if (g_is_silent_mode) return;
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (int)color);
-}
+void gotoxy(int x, int y);
+void hideCursor();
+void cls();
+extern bool g_is_silent_mode; //global variable to know if we are in silent mode
 
-void setCharColor(char c)
-{
-    if (!g_use_colors || g_is_silent_mode) return;
-
-    Color color = Color::WHITE;
-
-    switch (c)
-    {
-    case PLAYER1_CHAR:     
-        color = Color::LIGHTCYAN; 
-        break;
-    case PLAYER2_CHAR:
-        color = Color::DARKGREY;
-        break;
-    case BOMB_CHAR:
-        color = Color::LIGHTRED;
-        break;
-    case KEY_CHAR:
-    case TORCH_CHAR:
-        color = Color::YELLOW;
-        break;
-    case RIDDLE_CHAR:
-        color = Color::LIGHTMAGENTA;
-        break;
-        
-    default:
-        color = Color::LIGHTGREY;
-        break;
-    }
-
-    setTextColor(color);
-}
+//colors
+void setTextColor(Color color);
+extern bool g_use_colors;
+void setCharColor(char c);

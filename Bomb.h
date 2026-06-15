69a22ca46df0cb847589e6504455f806ba9a96d0
SanitaGasanov <sanitagasanov12@gmail.com>
2026-06-15 03:12:41 +0300

    ok

--- Bomb.h
+++ Bomb.h
@@ -1,90 +1,48 @@
-#include "Bomb.h"
-#include "Player.h"
-/////////////////////////////////////////////////////////////
-//activate the bomb at position (x,y)
-void Bomb::activate(const Point& drop_location, int room)
-{
-    location.setX(drop_location.getX());
-    location.setY(drop_location.getY());
+#pragma once
 
-    room_index = room;
-    is_active = true;
-    countdown = 5; // 5 game loops until explosion
-    location.draw();
-}
-//////////////////////////////////////////////////////////////
-void Bomb::update(Player& p1, Player& p2) {
-    if (!is_active) //if the bomb is not active, do nothing
+#include "Point.h"
+#include "Screen.h"
+class Player;
+
+
+class Bomb {
+private:
+    Point location;
+    int countdown = EXPLODE_TIMER;     // Timer in game loops (ticks)
+    bool is_active = false;// Flag to indicate if the bomb is active
+    int room_index = 0;
+    Screen& s;
+
+
+public:
+    /////////////////////////////////////////////////////////
+    //ctor
+    Bomb(const Point& p, Screen& screen) :location(p), s(screen), countdown(5), is_active(false), room_index(0)
     {
-        return;
+        location.setChar(BOMB_CHAR); // '@'
     }
-
-    if (countdown > 0) // every game loop, decrease countdown
+    ////////////////////////////////////////////////////////
+    //draw the bomb on the screen
+    void draw() const
     {
-        countdown--;
+        location.draw();
     }
+    ///////////////////////////////////////////////////////
+    //activate the bomb at position (x,y)
+    void activate(const Point& drop_location, int room); //activate the bomb at position (x,y)
+    ////////////////////////////////////////////////////////
+    //update the bomb status
+    void update(Player& p1, Player& p2);
+    ///////////////////////////////////////////////////////
+    // check if the bomb is active
+    bool isActive() const { return is_active; }
+    /////////////////////////////////////////////////////////
+    // get the bomb location
+    Point getLocation() const { return location; }
 
-    if (countdown == 0) // Trigger explosion when countdown hits zero
-    {
-        explode(p1, p2); //boom!
-        is_active = false; // the bomb is no longer active
-    }
-}
-
-void Bomb::explode(Player& p1, Player& p2)
-{
-    location.draw(' '); // erase the bomb from the screen
-    Point current_pos = location;
-    current_pos.setChar(' ');
-    s.setCharAt(current_pos, room_index);
-
-    checkDamagePlayer(p1, room_index);
-    checkDamagePlayer(p2, room_index);
-
-    // Check a 7x7 area centered on the bomb's location
-    for (int y = -EXPLOSION_RADIUS; y <= EXPLOSION_RADIUS; y++) {
-        for (int x = -EXPLOSION_RADIUS; x <= EXPLOSION_RADIUS; x++) {
-
-            Point target_pos = location;
-            target_pos.setX(location.getX() + x);
-            target_pos.setY(location.getY() + y);
-
-            // Check bounds
-            if (target_pos.getX() < 0 || target_pos.getX() >= MAX_X ||
-                target_pos.getY() < 0 || target_pos.getY() >= MAX_Y)
-            {
-                continue;
-            }
-
-            char char_at_target = s.getCharAt(target_pos, room_index);
-
-            //destroy:
-
-            if (char_at_target == WALL_CHAR)
-            {
-                target_pos.setChar(' ');
-                s.setCharAt(target_pos, room_index);
-                target_pos.draw(' ');
-            }
-
-
-        }
-    }
-
-}
-
-void Bomb::checkDamagePlayer(Player& p, int room)
-{
-    if (p.isAlive() && p.getCurrentRoomIndex() == room)
-    {
-        int dx = abs(p.getLocation().getX() - location.getX());
-        int dy = abs(p.getLocation().getY() - location.getY());
-
-        if (dx <= 3 && dy <= 3)
-        {
-            p.hitByBomb(); //Reduce player's life by one
-            s.setHealthAt(p, room); // Update health display
-        }
-    }
-
-}
+private:
+    /////////////////////////////////////////////////////////
+    // explode the bomb
+    void explode(Player& p1, Player& p2);
+    void checkDamagePlayer(Player& p, int room);
+};
